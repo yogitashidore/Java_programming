668efee8c4caa09dbe37456d27aa18c65dc6578c
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-12-25 22:57:41 +0530

    Create W_loop1.java

---
+++ W_loop1.java
@@ -0,0 +1,11 @@
+//print 1 to 100 number in java using while loop
+package mainjava;
+
+public class add {
+public static void main(String[] args) {
+	for(int i=1; i<=100; i++)
+	{
+	System.out.println(i);
+}
+}
+}
