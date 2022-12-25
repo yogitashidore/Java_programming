504e31c3ffbd119fdd03cd8819b53ffa016d5a06
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-12-25 22:51:24 +0530

    Create one_100_no.java

---
+++ one_100_no.java
@@ -0,0 +1,14 @@
+//print 1 to 100 number in java using for loop
+package mainjava;
+
+public class add {
+public static void main(String[] args) {
+	for(int i=1; i<=100; i++)
+	{
+	System.out.println(i);
+}
+}
+}
+
+
+//output: 1 2 3 .............. 100
