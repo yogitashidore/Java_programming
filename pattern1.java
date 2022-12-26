37287a93aec6db15b6630cc7be68aa35f76da8f7
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-12-26 12:56:16 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,16 @@
+//write a program to print below pattern in java 
+*****
+package mainjava;
+
+
+public class add {
+public static void main(String[] args) {
+	int i;
+		for(i = 1; i <= 5; i++)
+		{
+			System.out.print("*");
+		}
+
+}
+}
+
