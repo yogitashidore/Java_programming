4f9e0aed818377003bb884c8004936f617b95385
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-12-26 10:39:48 +0530

    Create min.java

---
+++ min.java
@@ -0,0 +1,21 @@
+//write a program to print min number in java using ternary operator.
+
+package mainjava;
+
+import java.util.Scanner;
+
+public class add {
+public static void main(String[] args) {
+	int a, b, c, d;
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter all three numbers:");
+	a = s.nextInt();
+	b = s.nextInt();
+	c = s.nextInt();
+	d = c <(a < b ? a : b)? c : ((a < b) ? a : b);
+	
+		System.out.println("Smaller Number :"+d);
+		
+	
+}
+}
