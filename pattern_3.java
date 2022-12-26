180fb43535dfd44ee33e1d3c2c754c3821f55c5f
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-12-26 13:42:56 +0530

    Create pattern_3.java

---
+++ pattern_3.java
@@ -0,0 +1,23 @@
+//write a program to print below pattern in java
+*
+**
+***
+package mainjava;
+
+
+public class add {
+public static void main(String[] args) {
+	int i, j, row=3;
+	
+		for(i = 0; i < row; i++)
+		{
+			for(j = 0; j <= i; j++)
+			{
+					System.out.print("*");
+			}
+			
+					System.out.println("");
+		}
+}	
+}
+	
