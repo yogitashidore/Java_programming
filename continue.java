32288f18e4d09bb4f30cdb452f6a9fe30f9b02db
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-12-26 10:05:19 +0530

    Create continue.java

---
+++ continue.java
@@ -0,0 +1,19 @@
+//write a program to demostrate usage of continue in java.
+package mainjava;
+
+public class add {
+public static void main(String[] args) {
+	int i;
+	for(i=0; i<=10; i++)
+	{
+		if(i==5)
+			continue;
+		System.out.println(i);
+		
+	
+}
+}
+}
+
+
+//output: 0 1 2 3 4 6 7 8 9 10
