55d438816340029e6499999a3acdc79989f7c4f1
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2022-12-26 10:00:49 +0530

    Create break.java

---
+++ break.java
@@ -0,0 +1,22 @@
+//write a program to demonstrate usage of break in java.
+package mainjava;
+
+public class add {
+public static void main(String[] args) {
+	int i=0;
+	while(i<=10)
+	{
+		System.out.println(i);
+		i++;
+		if(i==6)
+		{
+	       break;
+		}
+		
+	
+}
+}
+}
+
+
+//output: 0 1 2 3 4 5
