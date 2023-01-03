85062d23ec35b0540434207fc4fccc228a30a5ed
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2023-01-03 12:24:29 +0530

    Create pattern_5.java

---
+++ pattern_5.java
@@ -0,0 +1,30 @@
+//write a program to print below pattern
+***
+ **
+  *
+
+
+package mainjavaproject;
+
+public class pattern {
+	public static void main(String[] args)
+	{
+		for(int row=0; row<=2; row++)
+		{
+			for(int col= 0; col<=2; col++)
+			{
+				if(row<=col)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+	
+
+}
