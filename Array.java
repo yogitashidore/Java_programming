bd2a038b2cd3a2bc40880f136c6fb82a00ce9464
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2023-01-04 13:33:41 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,25 @@
+package mainjava;
+
+public class array {
+public static void main(String[] args)
+{
+	//create an array
+	int[] age = {12, 4, 5, 2,5};
+	
+	System.out.println("Accessing Elements of Array:");
+	System.out.println("First Element: " + age[0]);
+	System.out.println("Second Element: " + age[1]);
+	System.out.println("Third Element: " + age[2]);
+	System.out.println("Fourth Element: " + age[3]);
+	System.out.println("Fourth Element: " + age[4]);
+	
+}
+}
+
+
+//output:  Accessing Elements of Array:
+           First Element: 12
+           Second Element: 4
+           Third Element: 5
+           Fourth Element: 2
+           Fourth Element: 5
