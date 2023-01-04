472643b07b623d013853b05d949c4c6f8d589a7d
yogitashidore <111229626+yogitashidore@users.noreply.github.com>
2023-01-04 08:53:55 +0530

    Create pattern6.java

---
+++ pattern6.java
@@ -0,0 +1,25 @@
+//write a program to print pattern
+
+package mainjava;
+
+public class pattern {
+
+public static void main(String[]args)
+{
+	int i, j, k = 1;
+	for(i = 1; i<= 5; i++)
+	{
+		for(j = 1; j<i+1; j++)
+		{
+			System.out.print(k++ +" ");
+		}
+		System.out.println();
+	}
+}
+}
+
+//output: 1 
+          2 3 
+          4 5 6 
+          7 8 9 10 
+          11 12 13 14 15 
